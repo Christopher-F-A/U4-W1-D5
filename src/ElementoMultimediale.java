998cc72a3eaef2552a91195a5622b2386dde744d
Christopher F A <christopher.armani095@gmail.com>
2026-03-13 11:45:03 +0100

    Definizione struttura

---
+++ src/ElementoMultimediale.java
@@ -0,0 +1,7 @@
+public abstract class ElementoMultimediale {
+    public String titolo;
+
+    public ElementoMultimediale(String titolo) {
+        this.titolo = titolo;
+    }
+}
---
+++ src/RegistrazioneAudio.java
@@ -0,0 +1,9 @@
+public class RegistrazioneAudio extends ElementoMultimediale implements Riproducibile {
+    public int durata;
+    public int volume;
+
+    public RegistrazioneAudio(String titolo, int durata,  int volume; ) {
+        super(titolo);
+        this.durata = 4
+        this.volume = 6;
+    }}
